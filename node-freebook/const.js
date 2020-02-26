b4fa80a0d9687c5db3a70da02b973fa9037990b8
zhiyuIpos <zhiyu-@outlook.com>
2020-02-26 23:02:50 +0800

    version 3.0

--- node-freebook/const.js
+++ node-freebook/const.js
@@ -1,4 +1,10 @@
-const resUrl = 'http://192.168.0.106:8081'
+const env = require('env')
+let resUrl 
+if(env === 'dev'){
+    resUrl = 'http://192.168.0.106:8081'
+}else if(env === 'prood'){
+    resUrl = 'http://47.108.181.140'
+}
 
 const category=[
     'Biomedicine',
